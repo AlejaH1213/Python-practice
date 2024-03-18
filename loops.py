e987650e428da2a8f8dd2858f3e62d80ec987ad5
Aleja Hale <alejajara2@gmail.com>
2024-03-18 11:08:19 -0700

    loops practice

---
+++ loops.py
@@ -0,0 +1,44 @@
+python_topics = ["variables", "control flow", "loops", "modules", "classes"]
+
+length = len(python_topics)
+index = 0
+
+while index < length:
+  print("I am learning about " + python_topics[index])
+  index += 1
+#==============================================================================================
+
+
+#==============================================================================================
+dog_breeds_available_for_adoption = ["french_bulldog", "dalmatian", "shihtzu", "poodle", "collie"]
+dog_breed_I_want = "dalmatian"
+
+for dog_breed in dog_breeds_available_for_adoption:
+  print(dog_breed)
+  if dog_breed == dog_breed_I_want:
+    print("They have the dog I want!")
+    break
+
+ages = [12, 38, 34, 26, 21, 19, 67, 41, 17]
+# Loop through the ages list. If an entry is less than 21, skip it and move to the next entry. Otherwise, print() the age.
+for age in ages:
+  if age < 21:
+    continue
+  print(age)
+#==============================================================================================
+
+
+#==============================================================================================
+# Create a list called single_digits that consists of the numbers 0-9 (inclusive).
+single_digits = [0 , 1, 2, 3, 4, 5, 6, 7, 8, 9]
+# Create a for loop that goes through single_digits and prints out each one.
+# Before the loop, create a list called squares. Assign it to be an empty list to begin with.
+squares= []
+for num in single_digits:
+  # Inside the loop that iterates through single_digits, append the squared value of each element of single_digits to the list squares.
+  squares.append(num ** 2)
+  print(num)
+print(squares)
+# Create the list cubes using a list comprehension on the single_digits list. Each element of cubes should be an element of single_digits taken to the third power
+cubes = [num ** 3 for num in single_digits]
+print(cubes)
